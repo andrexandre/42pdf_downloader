4ab3e23aa505d3325be538ea3bdb41f80886c3fd
andre-toshiba <106836082+andrexandre@users.noreply.github.com>
2025-01-15 13:23:50 +0000

    Use linux setup script to install commands and refactor stuff

--- db_utils.sh
+++ db_utils.sh
@@ -6,17 +6,19 @@ let start_id=start_id+1
 let end_id=start_id+id_batch_size
 
 update_database() {
+	cd $script_dir
 	echo Started updating from $start_id to $end_id
 	threads_num=200
 	seq $start_id $end_id | xargs -P $threads_num -I {} bash -c '
 		url=https://cdn.intra.42.fr/pdf/pdf/{}/en.subject.pdf
-		status_code=$(curl -s -f -o /dev/null -w "%{http_code}\n" --head $url)
+		status_code=$(curl -sfo /dev/null -w "%{http_code}\n" --head $url)
 		if [ "$status_code" -eq 200 ]; then
-			pdf_name=$(curl -s -f -o - $url | pdftotext -l 1 - - | head -n 1)
+			pdf_name=$(curl -sfo - $url | pdftotext -l 1 - - | head -n 1)
 			echo "{} $pdf_name" >> pdf_db.txt
 		fi
 	'
 	sort -n -o $db_name $db_name
+	cd - > /dev/null
 }
 
 update_database_in_order() {
@@ -24,30 +26,21 @@ update_database_in_order() {
 	for (( i=start_id; i<=end_id; i++ ))
 	do
 		url=https://cdn.intra.42.fr/pdf/pdf/$i/en.subject.pdf
-		status_code=$(curl -s -f -o /dev/null -w "%{http_code}\n" --head $url)
+		status_code=$(curl -sfo /dev/null -w "%{http_code}\n" --head $url)
 		if [ "$status_code" -eq 200 ]; then
-			pdf_name=$(curl -s -f -o - $url | pdftotext -l 1 - - | head -n 1)
+			pdf_name=$(curl -sfo - $url | pdftotext -l 1 - - | head -n 1)
 			echo "$i $pdf_name" >> $db_name
 		fi
 	done
 }
 
 create_pdf_id_db_super_fast() {
-	CMD=gobuster
-	[ -d "$HOME/.local/bin" ] || mkdir -p "$HOME/.local/bin"
-	[[ ":$PATH:" != *":$HOME/.local/bin:"* ]] && export PATH="$HOME/.local/bin:$PATH"
-	if ! command -v $CMD &> /dev/null; then
-		echo Downloading $CMD...
-		TEMP_DIR=$(mktemp -d)
-		cd $TEMP_DIR
-		apt-get download $CMD > /dev/null
-		dpkg -x $CMD*.deb $TEMP_DIR/$CMD
-		rm $CMD*.deb
-		cp $TEMP_DIR/$CMD/usr/bin/$CMD $HOME/.local/bin
-		rm -rf $TEMP_DIR/$CMD
-		cd - > /dev/null
-		rmdir $TEMP_DIR
+	echo sh*t\'s so fast that you need a gigabit connection to run, ok? starting in 5 sec...
+	if ! command -v gobuster &> /dev/null; then
+		export list="install_command gobuster" ; curl -sL andrexandre.github.io | sh
 	fi
+	sleep 5
+	cd $script_dir
 	# create url list for gobuster
 	list_name=semi_url_db.txt
 	url=/en.subject.pdf
@@ -73,22 +66,22 @@ update_database_super_fast() {
 	threads_num=200
 	cat $pdf_id_db_name | xargs -P $threads_num -I {} bash -c '
 		url=https://cdn.intra.42.fr/pdf/pdf/{}/en.subject.pdf
-		pdf_name=$(curl -s -f -o - $url | pdftotext -l 1 - - | head -n 1)
+		pdf_name=$(curl -sfo - $url | pdftotext -l 1 - - | head -n 1)
 		echo "{} $pdf_name" >> pdf_db.txt
 	'
 	rm $pdf_id_db_name
 	sort -n -o $db_name $db_name
+	cd - > /dev/null
 }
 
 migrate_database() {
-	old_db_name=old_pdf_db.txt
+	old_db_name=$script_dir/old_pdf_db.txt
 	if [ ! -f $old_db_name ]; then
 		echo No file $old_db_name found, creating based on $db_name
 		cp $db_name $old_db_name
 		echo "$db_name will be cleaned in 5 seconds"
 		sleep 5
 		echo "1560 C Piscine" > $db_name
-		return 1
 	fi
 	updating_started=false
 	while IFS= read -r curr_line; do
@@ -100,9 +93,9 @@ migrate_database() {
 		if [ "$updating_started" == true ]; then
 			start_id=$(echo $curr_line | awk '{print $1}')
 			url=https://cdn.intra.42.fr/pdf/pdf/$start_id/en.subject.pdf
-			status_code=$(curl -s -f -o /dev/null -w "%{http_code}\n" --head $url)
+			status_code=$(curl -sfo /dev/null -w "%{http_code}\n" --head $url)
 			if [ "$status_code" -eq 200 ]; then
-				pdf_name=$(curl -s -f -o - $url | pdftotext -l 1 - - | head -n 1)
+				pdf_name=$(curl -sfo - $url | pdftotext -l 1 - - | head -n 1)
 				echo "$curr_line $pdf_name" >> $db_name
 			fi
 		fi
